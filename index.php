3b1d214aa9d417601ae2acc70f56068a7d1ab8cd
Marcap00 <cappellatomarco@yahoo.com>
2024-10-02 16:37:46 +0200

    linked stylesheet and created and styled elements on result.php page

--- index.php
+++ index.php
@@ -9,24 +9,31 @@
     <link href="https://fonts.googleapis.com/css?family=Poppins:100,100italic,200,200italic,300,300italic,regular,italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic" rel="stylesheet" />
     <!-- Bootstrap 5 -->
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css' integrity='sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==' crossorigin='anonymous' />
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 
 <body>
+    <!-- Container -->
     <div class="container">
+        <!-- Title e descrizione -->
         <div class="title-description">
             <h1 class="fw-semibold">PHP Badwords</h1>
             <p class="fs-3">Inserisci un testo e una parola che vuoi censurare</p>
         </div>
-
-        <form action="result.php">
+        <!-- Form con metodo GET -->
+        <form action="./result.php" method="GET">
+            <!-- Input dove l'utente inserirà il testo  -->
             <div class="mb-3">
                 <label for="input-text" class="form-label fs-5">Testo:</label>
                 <textarea class="form-control w-50" id="input-text" name="InputText" rows="3" placeholder="Text"></textarea>
             </div>
+            <!-- Input dove l'utente inserirà la parola da censurare -->
             <div class="mb-3">
                 <label for="badword" class="form-label fs-5 text-danger">Parola da censurare:</label>
-                <input type="text" class="form-control w-50" id="badword" name="badword" placeholder="Badword">
+                <input type="text" class="form-control w-50 text-danger" id="badword" name="badword" placeholder="Badword">
             </div>
+            <!-- Bottone per inviare i dati -->
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
     </div>
---
+++ result.php
@@ -0,0 +1,66 @@
+<?php
+// Creo le variabili
+// Testo inserito dall'utente
+$input_text = $_GET["InputText"];
+// Lunghezza del testo inserito
+$text_length = strlen($input_text);
+// Parola da censurare
+$badword = $_GET["badword"];
+// Uso la funzione str_ireplace per censurare la parola
+/* 
+Str-ireplace è case-insensitive accetta come parametri:
+1. La parola da cercare
+2. La stringa con cui sostituire la parola cercata
+3. La stringa di parole in cui cercare la parola da voler sostituire
+*/
+$censored_text = str_ireplace($badword, "***", $input_text);
+// Lunghezza del testo censurato
+$censored_text_length = strlen($censored_text);
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords</title>
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,100italic,200,200italic,300,300italic,regular,italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic" rel="stylesheet" />
+    <!-- Bootstrap 5 -->
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css' integrity='sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==' crossorigin='anonymous' />
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+    <!-- Container -->
+    <div class="container">
+        <!-- Title -->
+        <div class="title-description">
+            <h1 class="fw-semibold">PHP Badwords Result</h1>
+        </div>
+        <!-- Card del testo inserito -->
+        <div class="card w-50 p-3 mb-5">
+            <h3 class="fw-semibold ms-1">Testo inserito:</h3>
+            <div class="card p-3 mb-2">
+                <!-- Testo che ha inserito l'utente -->
+                <p class="mb-0"><?php echo $input_text ?></p>
+            </div>
+            <!-- Lunghezza del testo inserito -->
+            <h4 class="fw-semibold ms-1">Lunghezza del testo: <?php echo $text_length ?></h4>
+        </div>
+        <!-- Card del testo censurato -->
+        <div class="card w-50 p-3">
+            <h3 class="fw-semibold text-danger ms-1">Testo censurato:</h3>
+            <div class="card p-3 mb-2">
+                <!-- Testo censurato -->
+                <p class="mb-0"><?php echo $censored_text ?></p>
+            </div>
+            <!-- Lunghezza del testo censurato -->
+            <h4 class="fw-semibold ms-1">Lunghezza del testo: <?php echo $censored_text_length ?></h4>
+        </div>
+    </div>
+</body>
+
+</html>
